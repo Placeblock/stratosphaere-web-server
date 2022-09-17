d81b16c61866763a42363ec8ce9011de3583eaba
Placeblock <felix.weglehner@gmx.de>
2022-09-17 13:46:46 +0200

    Finished Auth and JWT generation Started implementing routers

--- service/auth_service/auth.go
+++ service/auth_service/auth.go
@@ -1,19 +1,14 @@
 package auth_service
 
 import (
-	"github.com/go-playground/validator/v10"
+	"stratosphaere-server/models"
 )
 
 type Auth struct {
-	Username string `max=20,required`
-	Password string `max=50,required`
-}
-
-func (a *Auth) Validate() error {
-	validation := validator.New()
-	return validation.Struct(a.Username)
+	Username string
+	Password string
 }
 
 func (a *Auth) Check() (bool, error) {
-
+	return models.CheckAuth(a.Username, a.Password)
 }
